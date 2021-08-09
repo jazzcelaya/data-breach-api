21a8be0d4307bb45d6703b0ea2ed69f7f240bdd6
Jazz <jazzcelaya@gmail.com>
2021-08-08 23:34:42 -0500

    FEATURE: adds fucntionality to /breaches endpoint

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const express = require("express");
+const express = require('express');
+const env = require('dotenv').config();
+const axios = require('axios');
 const app = express();
 let port = process.env.PORT || 8000;
+let KEY = process.env.haveibeenpwned_key;
 
 app.get("/", (req, res ) =>{
-    res.send("Hello World! :)")
+    res.send('Hello World! :)')
 })
 
-app.get("/breach", (req, res) => {
-    res.send("This endpoint will be where the good stuff happens")
+app.get("/breach/:account", (req, res) => {
+    const account = encodeURIComponent(req.params.account)
+    const headers = {"hibp-api-key": KEY }
+    const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${account}`
+
+    axios({
+        method: 'GET',
+        url: url,
+        headers: headers
+    }).then(response =>{
+        const breachesArray = response.data
+        const breachesExtent = breachesArray.length
+        if(breachesExtent < 1){ 
+            res.send(
+                'Awesome! Your account has not been involved on any data breach that we know of ;)'
+                )
+            return
+        } else if(breachesExtent === 1){ 
+            res.send('Your account has been involved in 1 (one) data breach')
+        } else{
+        res.send(`Your account has been involved in ${breachesExtent} breaches`)
+        }
+    })
+    .catch(error =>{
+        res.status(400)
+        res.send('There has been an error processing your request. Please verify your request')
+        console.log(account, error)
+    })
+    
 })
 
 app.listen(port, () => {
